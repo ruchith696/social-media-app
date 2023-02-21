505681e5281e85caeb6f6e351173669d6c289ebd
ruchith696 <ruchithreddy21@gmail.com>
2023-02-21 17:55:27 +0530

    done for day 21-02-23

--- socialmedia_frontend/src/container/Home.jsx
+++ socialmedia_frontend/src/container/Home.jsx
@@ -5,11 +5,60 @@ import { Link, Route, Routes } from "react-router-dom";
 
 import { Sidebar, UserProfile } from "../components";
 import Pins from "./Pins";
+import { userQuery } from "../utils/data";
 import { client } from "../client";
 import logo from "../assets/logo.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const userInfo =
+    localStorage.getItem("user") !== "undefined"
+      ? localStorage.getItem("user")
+      : localStorage.clear();
+
+  useEffect(() => {
+    const query = userQuery(JSON.parse(userInfo)?.sub);
+    console.log(query);
+    client.fetch(query).then((data) => {
+      console.log(data);
+      setUser(data[0]);
+    });
+  }, []);
+
+  return (
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
+      <div className="hidden md:flex h-screen flex-initial">
+        <Sidebar />
+      </div>
+      <div className="flex md:hidden flex-row">
+        <HiMenu
+          fontSize={40}
+          className="cursor-pointer"
+          onClick={() => setToggleSidebar(true)}
+        />
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28" />
+        </Link>
+        <Link to={`user-profile/${user?._id}`}>
+          <img src={user?.image} alt="logo" className="w-28" />
+        </Link>
+      </div>
+      {toggleSidebar && (
+        <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in ">
+          <div className="absolute w-full flex jsutify-end items-center p-2">
+            <AiFillCloseCircle
+              fontSize={30}
+              className="cursor-pointer"
+              onCLick={() => setToggleSidebar(false)}
+            />
+          </div>
+          <Sidebar />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
