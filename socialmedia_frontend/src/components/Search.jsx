f9a6e7f32328d7a1d5ff53699967946163a1ebc6
ruchith696 <ruchithreddy21@gmail.com>
2023-03-30 14:41:10 +0530

    Done with everything

--- socialmedia_frontend/src/components/Search.jsx
+++ socialmedia_frontend/src/components/Search.jsx
@@ -9,7 +9,31 @@ const Search = ({ searchTerm }) => {
   const [pins, setPins] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  return <div>Search</div>;
+  useEffect(() => {
+    if (searchTerm) {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+
+  return (
+    <div>
+      {loading && <Spinner message="Searching for pins" />}
+      {pins?.length > 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== "" && !loading && (
+        <div className="mt-10 text-center text-xl">No pins Found!</div>
+      )}
+    </div>
+  );
 };
 
 export default Search;
